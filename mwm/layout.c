michaelforney/velox

commit eb9c3b186182913d350099b464e190cc841b9179

Convert mwm to use loops to store windows
Also, tags now represent their focus with a pointer to the loop of their
windows.

In order to implement this cleanly, tags now store a list of their windows and
I have removed all parts relating to tag masks.

This means that for now, there will be no support for tags visible on multiple
windows or multiple tags visible at the same time. I have some ideas about how
this could be implemented, but for now, I'd rather go with simpler code, as I
do not use this feature.

mwm/layout.c
@@ -30,11 +30,11 @@
 
 struct mwm_hashtable * layouts;
 
-void tile_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_state)
+void tile_arrange(struct mwm_loop * windows, struct mwm_layout_state * generic_state)
 {
     struct mwm_tile_layout_state * state = (struct mwm_tile_layout_state *) generic_state;
     struct mwm_window * window = NULL;
-    struct mwm_list * iterator = NULL;
+    struct mwm_loop * iterator = NULL;
     uint16_t mask;
     uint32_t values[4];
     uint16_t window_count = 0;
@@ -55,13 +55,16 @@ void tile_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_s
     }
 
     /* Calculate number of windows */
-    for (iterator = windows; iterator != NULL; iterator = iterator->next)
+    iterator = windows;
+    do
     {
         if (!((struct mwm_window *) iterator->data)->floating)
         {
             window_count++;
         }
-    }
+
+        iterator = iterator->next;
+    } while (iterator != windows);
 
     printf("window_count: %i\n", window_count);
 
@@ -77,70 +80,71 @@ void tile_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_s
     }
 
     /* Arrange the windows */
-    for (iterator = windows, window_index = 0; iterator != NULL; iterator = iterator->next)
+    iterator = windows;
+    window_index = 0;
+    do
     {
         window = (struct mwm_window *) iterator->data;
 
-        if (window->floating)
+        if (!window->floating)
         {
-            continue;
-        }
-
-        if (window_index < state->master_count) /* Arranging a master */
-        {
-            window->x = 0;
-            window->y = window_index * screen_height / MIN(state->master_count, window_count);
-            window->width = ((window_count <= state->master_count) ? screen_width : state->master_factor * screen_width) - (2 * window->border_width);
-            window->height = screen_height / MIN(state->master_count, window_count) - (2 * window->border_width);
-        }
-        else /* Arranging the rest of the windows */
-        {
-            if (row_index == row_count)
+            if (window_index < state->master_count) /* Arranging a master */
             {
-                row_index = 0;
-                column_index++;
-                if ((window_count - state->master_count) % state->column_count == 0)
+                window->x = 0;
+                window->y = window_index * screen_height / MIN(state->master_count, window_count);
+                window->width = ((window_count <= state->master_count) ? screen_width : state->master_factor * screen_width) - (2 * window->border_width);
+                window->height = screen_height / MIN(state->master_count, window_count) - (2 * window->border_width);
+            }
+            else /* Arranging the rest of the windows */
+            {
+                if (row_index == row_count)
                 {
-                    row_count = (window_count - state->master_count) / state->column_count;
+                    row_index = 0;
+                    column_index++;
+                    if ((window_count - state->master_count) % state->column_count == 0)
+                    {
+                        row_count = (window_count - state->master_count) / state->column_count;
+                    }
+                    else
+                    {
+                        row_count = ((window_count - state->master_count) / state->column_count) + ((column_index < ((window_count - state->master_count) % state->column_count)) ? 1 : 0);
+                    }
                 }
-                else
-                {
-                    row_count = ((window_count - state->master_count) / state->column_count) + ((column_index < ((window_count - state->master_count) % state->column_count)) ? 1 : 0);
-                }
+
+                window->x = ((state->master_count == 0) ? 0 : state->master_factor * screen_width) + column_index * column_width;
+                window->y = screen_height * row_index / row_count;
+                window->width = column_width - (2 * window->border_width);
+                window->height = screen_height / row_count - (2 * window->border_width);
+
+                row_index++;
             }
 
-            window->x = ((state->master_count == 0) ? 0 : state->master_factor * screen_width) + column_index * column_width;
-            window->y = screen_height * row_index / row_count;
-            window->width = column_width - (2 * window->border_width);
-            window->height = screen_height / row_count - (2 * window->border_width);
+            mask = XCB_CONFIG_WINDOW_X |
+                   XCB_CONFIG_WINDOW_Y |
+                   XCB_CONFIG_WINDOW_WIDTH |
+                   XCB_CONFIG_WINDOW_HEIGHT;
 
-            row_index++;
+            values[0] = window->x;
+            values[1] = window->y;
+            values[2] = window->width;
+            values[3] = window->height;
+
+            printf("arranging window: %i (x: %i, y: %i, width: %i, height: %i)\n", window->window_id, window->x, window->y, window->width, window->height);
+
+            xcb_configure_window(c, window->window_id, mask, values);
+            synthetic_configure(window);
         }
 
-        mask = XCB_CONFIG_WINDOW_X |
-               XCB_CONFIG_WINDOW_Y |
-               XCB_CONFIG_WINDOW_WIDTH |
-               XCB_CONFIG_WINDOW_HEIGHT;
-
-        values[0] = window->x;
-        values[1] = window->y;
-        values[2] = window->width;
-        values[3] = window->height;
-
-        printf("arranging window: %i (x: %i, y: %i, width: %i, height: %i)\n", window->window_id, window->x, window->y, window->width, window->height);
-
-        xcb_configure_window(c, window->window_id, mask, values);
-        synthetic_configure(window);
-
         window_index++;
-    }
+
+        iterator = iterator->next;
+    } while (iterator != windows);
 }
 
-void grid_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_state)
+void grid_arrange(struct mwm_loop * windows, struct mwm_layout_state * generic_state)
 {
-    struct mwm_tile_layout_state * state = (struct mwm_tile_layout_state *) generic_state;
     struct mwm_window * window = NULL;
-    struct mwm_list * iterator = NULL;
+    struct mwm_loop * iterator = NULL;
     uint16_t mask;
     uint32_t values[4];
     uint16_t window_count = 0;
@@ -162,13 +166,16 @@ void grid_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_s
     }
 
     /* Calculate number of windows */
-    for (iterator = windows; iterator != NULL; iterator = iterator->next)
+    iterator = windows;
+    do
     {
         if (!((struct mwm_window *) iterator->data)->floating)
         {
             window_count++;
         }
-    }
+
+        iterator = iterator->next;
+    } while (iterator != windows);
 
     printf("window_count: %i\n", window_count);
 
@@ -185,53 +192,54 @@ void grid_arrange(struct mwm_list * windows, struct mwm_layout_state * generic_s
     }
 
     /* Arrange the windows */
-    for (iterator = windows, window_index = 0; iterator != NULL; iterator = iterator->next)
+    iterator = windows;
+    window_index = 0;
+    do
     {
         window = (struct mwm_window *) iterator->data;
 
-        if (window->floating)
+        if (!window->floating)
         {
-            continue;
-        }
-
-        if (row_index == row_count)
-        {
-            row_index = 0;
-            column_index++;
-            if (window_count % column_count == 0)
+            if (row_index == row_count)
             {
-                row_count = window_count / column_count;
+                row_index = 0;
+                column_index++;
+                if (window_count % column_count == 0)
+                {
+                    row_count = window_count / column_count;
+                }
+                else
+                {
+                    row_count = (window_count / column_count) + ((column_index < (window_count % column_count)) ? 1 : 0);
+                }
             }
-            else
-            {
-                row_count = (window_count / column_count) + ((column_index < (window_count % column_count)) ? 1 : 0);
-            }
+
+            window->x = column_index * column_width;
+            window->y = screen_height * row_index / row_count;
+            window->width = column_width - (2 * window->border_width);
+            window->height = screen_height / row_count - (2 * window->border_width);
+
+            row_index++;
+
+            mask = XCB_CONFIG_WINDOW_X |
+                   XCB_CONFIG_WINDOW_Y |
+                   XCB_CONFIG_WINDOW_WIDTH |
+                   XCB_CONFIG_WINDOW_HEIGHT;
+
+            values[0] = window->x;
+            values[1] = window->y;
+            values[2] = window->width;
+            values[3] = window->height;
+
+            printf("arranging window: %i (x: %i, y: %i, width: %i, height: %i)\n", window->window_id, window->x, window->y, window->width, window->height);
+
+            xcb_configure_window(c, window->window_id, mask, values);
+            synthetic_configure(window);
         }
-
-        window->x = column_index * column_width;
-        window->y = screen_height * row_index / row_count;
-        window->width = column_width - (2 * window->border_width);
-        window->height = screen_height / row_count - (2 * window->border_width);
-
-        row_index++;
-
-        mask = XCB_CONFIG_WINDOW_X |
-               XCB_CONFIG_WINDOW_Y |
-               XCB_CONFIG_WINDOW_WIDTH |
-               XCB_CONFIG_WINDOW_HEIGHT;
-
-        values[0] = window->x;
-        values[1] = window->y;
-        values[2] = window->width;
-        values[3] = window->height;
-
-        printf("arranging window: %i (x: %i, y: %i, width: %i, height: %i)\n", window->window_id, window->x, window->y, window->width, window->height);
-
-        xcb_configure_window(c, window->window_id, mask, values);
-        synthetic_configure(window);
 
         window_index++;
-    }
+        iterator = iterator->next;
+    } while (iterator != windows);
 }
 
 void setup_layouts()

libmwm/list.c
@@ -1,3 +1,5 @@
+// vim: fdm=syntax fo=croql noet sw=4 sts=4 ts=8
+
 /* mwm: libmwm/list.c
  *
  * Copyright (c) 2010 Michael Forney <michael@obberon.com>
@@ -207,6 +209,11 @@ void mwm_list_swap(struct mwm_list * first, struct mwm_list * second)
 
     assert(first);
     assert(second);
+
+    if (first == second)
+    {
+        return;
+    }
 
     data = first->data;
 

libmwm/list.h
@@ -1,3 +1,5 @@
+// vim: fdm=syntax fo=croql sw=4 sts=4 ts=8
+
 /* mwm: libmwm/list.h
  *
  * Copyright (c) 2010 Michael Forney <michael@obberon.com>

libmwm/loop.c
@@ -21,6 +21,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "loop.h"
 
@@ -75,7 +76,7 @@ struct mwm_loop * mwm_loop_remove(struct mwm_loop * loop)
     struct mwm_loop * new_loop;
 
     /* If there is only one element in the loop */
-    if (loop == loop->next && loop == loop->previous)
+    if (mwm_loop_is_singleton(loop))
     {
         free(loop);
         return NULL;
@@ -91,6 +92,15 @@ struct mwm_loop * mwm_loop_remove(struct mwm_loop * loop)
     return new_loop;
 }
 
+/**
+ * Delete an entire loop, and optionally free the data
+ *
+ * This is an O(n) operation
+ *
+ * @param loop The loop to delete
+ * @param free_data Whether or not to free the data enclosed in the loop
+ * @return A new pointer to the loop, in this case NULL
+ */
 struct mwm_loop * mwm_loop_delete(struct mwm_loop * loop, bool free_data)
 {
     while (loop != NULL)
@@ -104,5 +114,74 @@ struct mwm_loop * mwm_loop_delete(struct mwm_loop * loop, bool free_data)
     }
 
     return NULL;
+}
+
+/**
+ * Copies an entire loop
+ *
+ * This is an O(n) operation
+ *
+ * @param loop The loop to copy
+ * @return A copy of the loop
+ */
+struct mwm_loop * mwm_loop_copy(struct mwm_loop * loop)
+{
+    if (loop == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        struct mwm_loop * iterator;
+        struct mwm_loop * new_loop, * element;
+
+        iterator = loop;
+
+        do
+        {
+            element = (struct mwm_loop *) malloc(sizeof(struct mwm_loop));
+            new_loop = mwm_loop_insert(new_loop, element);
+            iterator = iterator->next;
+        } while (iterator != loop);
+
+        return new_loop;
+    }
+}
+
+/**
+ * Swap two elements of a loop
+ *
+ * This is an O(1) operation
+ *
+ * @param first The first element to swap
+ * @param second The second element to swap
+ */
+void mwm_loop_swap(struct mwm_loop * first, struct mwm_loop * second)
+{
+    void * data;
+
+    assert(first);
+    assert(second);
+
+    if (first == second)
+    {
+        return;
+    }
+
+    data = first->data;
+
+    first->data = second->data;
+    second->data = data;
+}
+
+/**
+ * Determines whether or not a loop contains a single element
+ *
+ * @param loop The loop to check
+ * @return True if the loop contains a single element, false otherwise
+ */
+bool mwm_loop_is_singleton(struct mwm_loop * loop)
+{
+    return (loop == loop->previous) && (loop == loop->next);
 }
 

libmwm/loop.h
@@ -34,6 +34,9 @@ struct mwm_loop
 struct mwm_loop * mwm_loop_insert(struct mwm_loop * loop, void * data);
 struct mwm_loop * mwm_loop_remove(struct mwm_loop * loop);
 struct mwm_loop * mwm_loop_delete(struct mwm_loop * loop, bool free_data);
+struct mwm_loop * mwm_loop_copy(struct mwm_loop * loop);
+void mwm_loop_swap(struct mwm_loop * first, struct mwm_loop * second);
+bool mwm_loop_is_singleton(struct mwm_loop * loop);
 
 #endif
 

mwm/layout.h
@@ -22,7 +22,7 @@
 
 #include <stdint.h>
 
-#include "list.h"
+#include "loop.h"
 #include "hashtable.h"
 
 struct mwm_layout_state
@@ -41,7 +41,7 @@ struct mwm_tile_layout_state
 struct mwm_layout
 {
     const char * identifier;
-    void (* arrange)(struct mwm_list * list, struct mwm_layout_state * state);
+    void (* arrange)(struct mwm_loop * list, struct mwm_layout_state * state);
     struct mwm_layout_state default_state;
 };
 

mwm/mwm.h
@@ -34,9 +34,6 @@ extern uint16_t screen_height;
 
 extern xcb_atom_t WM_PROTOCOLS, WM_DELETE_WINDOW, WM_STATE;
 
-extern struct mwm_list * visible_windows;
-extern struct mwm_list * hidden_windows;
-
 void synthetic_configure();
 
 void arrange();

commit 542d126fc2454bfc40a295a721a9ff3f20750721

Fix bug with floating window introduced in eb9c3b18

mwm/layout.c
@@ -133,9 +133,9 @@ void tile_arrange(struct mwm_loop * windows, struct mwm_layout_state * generic_s
 
             xcb_configure_window(c, window->window_id, mask, values);
             synthetic_configure(window);
-        }
 
-        window_index++;
+            window_index++;
+        }
 
         iterator = iterator->next;
     } while (iterator != windows);
@@ -235,9 +235,10 @@ void grid_arrange(struct mwm_loop * windows, struct mwm_layout_state * generic_s
 
             xcb_configure_window(c, window->window_id, mask, values);
             synthetic_configure(window);
+
+            window_index++;
         }
 
-        window_index++;
         iterator = iterator->next;
     } while (iterator != windows);
 }