637cf6f7b8cf851b13cb28a30fb9b439e13ff3d4
Michael Forney <michael@obberon.com>
2010-02-16 21:42:35 -0800

    Sort events by event id

--- velox/event.c
+++ velox/event.c
@@ -37,9 +37,158 @@
 #define CLEAN_MASK(mask) (mask & ~(mod_mask_numlock | XCB_MOD_MASK_LOCK))
 
 /* X event handlers */
+static void key_press(xcb_key_press_event_t * event)
+{
+    xcb_keysym_t keysym = 0;
+    struct velox_list * iterator;
+    struct velox_key_binding * binding;
+
+    keysym = xcb_get_keyboard_mapping_keysyms(keyboard_mapping)[keyboard_mapping->keysyms_per_keycode * (event->detail - xcb_get_setup(c)->min_keycode)];
+
+    DEBUG_PRINT("keysym: %x\n", keysym)
+    DEBUG_PRINT("modifiers: %i\n", event->state)
+
+    for (iterator = key_bindings; iterator != NULL; iterator = iterator->next)
+    {
+        binding = (struct velox_key_binding *) iterator->data;
+
+        if (keysym == binding->key->keysym &&
+            ((binding->key->modifiers == XCB_MOD_MASK_ANY) ||
+            (CLEAN_MASK(event->state) == binding->key->modifiers)))
+        {
+            if (binding->function != NULL)
+            {
+                binding->function(binding->arg);
+            }
+        }
+    }
+}
+
 static void button_press(xcb_button_press_event_t * event)
 {
     DEBUG_ENTER
+}
+
+static void enter_notify(xcb_enter_notify_event_t * event)
+{
+    DEBUG_ENTER
+    DEBUG_PRINT("window_id: %i\n", event->event)
+
+    if (event->event == root) focus(root);
+    else
+    {
+        struct velox_loop * element;
+
+        element = window_loop_locate(tag->windows, event->event);
+
+        if (element != NULL)
+        {
+            struct velox_window * window;
+
+            window = (struct velox_window *) element->data;
+            DEBUG_PRINT("mode: %i\n", event->mode)
+            DEBUG_PRINT("detail: %i\n", event->detail)
+
+            if (event->mode == XCB_NOTIFY_MODE_NORMAL && event->detail != XCB_NOTIFY_DETAIL_INFERIOR)
+            {
+                focus(window->window_id);
+                window->tag->focus = element;
+            }
+        }
+    }
+}
+
+static void leave_notify(xcb_leave_notify_event_t * event)
+{
+    DEBUG_ENTER
+}
+
+static void destroy_notify(xcb_destroy_notify_event_t * event)
+{
+    struct velox_window * window;
+
+    DEBUG_ENTER
+    DEBUG_PRINT("window_id: %i\n", event->window)
+
+    window = tags_lookup_window(event->window);
+
+    if (window != NULL)
+    {
+        unmanage(window);
+    }
+}
+
+static void unmap_notify(xcb_unmap_notify_event_t * event)
+{
+    struct velox_window * window;
+
+    DEBUG_ENTER
+
+    if (pending_unmaps > 0)
+    {
+        /* If we are expecting an unmap due to hiding a window, ignore it */
+        pending_unmaps--;
+        return;
+    }
+
+    window = window_loop_lookup(tag->windows, event->window);
+
+    if (window != NULL)
+    {
+        uint32_t property_values[2];
+
+        DEBUG_PRINT("setting state to withdrawn\n")
+
+        xcb_grab_server(c);
+
+        property_values[0] = XCB_WM_STATE_WITHDRAWN;
+        property_values[1] = 0;
+        xcb_change_property(c, XCB_PROP_MODE_REPLACE, window->window_id, WM_STATE, WM_STATE, 32, 2, property_values);
+
+        unmanage(window);
+
+        xcb_flush(c);
+
+        xcb_ungrab_server(c);
+    }
+}
+
+static void map_request(xcb_map_request_event_t * event)
+{
+    struct velox_window * maybe_window;
+    xcb_get_window_attributes_cookie_t window_attributes_cookie;
+    xcb_get_window_attributes_reply_t * window_attributes;
+
+    DEBUG_ENTER
+
+    window_attributes_cookie = xcb_get_window_attributes(c, event->window);
+
+    maybe_window = tags_lookup_window(event->window);
+
+    window_attributes = xcb_get_window_attributes_reply(c, window_attributes_cookie, NULL);
+
+    if (window_attributes)
+    {
+        if (!maybe_window && !window_attributes->override_redirect)
+        {
+            manage(event->window);
+        }
+
+        free(window_attributes);
+    }
+}
+
+static void configure_notify(xcb_configure_notify_event_t * event)
+{
+    DEBUG_ENTER
+
+    if (event->window == root)
+    {
+        screen_area.width = event->width;
+        screen_area.height = event->height;
+
+        run_hooks(NULL, VELOX_HOOK_ROOT_RESIZED);
+    }
 }
 
 static void configure_request(xcb_configure_request_event_t * event)
@@ -102,130 +251,6 @@ static void configure_request(xcb_configure_request_event_t * event)
     xcb_flush(c);
 }
 
-static void configure_notify(xcb_configure_notify_event_t * event)
-{
-    DEBUG_ENTER
-
-    if (event->window == root)
-    {
-        screen_area.width = event->width;
-        screen_area.height = event->height;
-
-        run_hooks(NULL, VELOX_HOOK_ROOT_RESIZED);
-    }
-}
-
-static void destroy_notify(xcb_destroy_notify_event_t * event)
-{
-    struct velox_window * window;
-
-    DEBUG_ENTER
-    DEBUG_PRINT("window_id: %i\n", event->window)
-
-    window = tags_lookup_window(event->window);
-
-    if (window != NULL)
-    {
-        unmanage(window);
-    }
-}
-
-static void enter_notify(xcb_enter_notify_event_t * event)
-{
-    DEBUG_ENTER
-    DEBUG_PRINT("window_id: %i\n", event->event)
-
-    if (event->event == root) focus(root);
-    else
-    {
-        struct velox_loop * element;
-
-        element = window_loop_locate(tag->windows, event->event);
-
-        if (element != NULL)
-        {
-            struct velox_window * window;
-
-            window = (struct velox_window *) element->data;
-            DEBUG_PRINT("mode: %i\n", event->mode)
-            DEBUG_PRINT("detail: %i\n", event->detail)
-
-            if (event->mode == XCB_NOTIFY_MODE_NORMAL && event->detail != XCB_NOTIFY_DETAIL_INFERIOR)
-            {
-                focus(window->window_id);
-                window->tag->focus = element;
-            }
-        }
-    }
-}
-
-static void leave_notify(xcb_leave_notify_event_t * event)
-{
-    DEBUG_ENTER
-}
-
-static void key_press(xcb_key_press_event_t * event)
-{
-    xcb_keysym_t keysym = 0;
-    struct velox_list * iterator;
-    struct velox_key_binding * binding;
-
-    keysym = xcb_get_keyboard_mapping_keysyms(keyboard_mapping)[keyboard_mapping->keysyms_per_keycode * (event->detail - xcb_get_setup(c)->min_keycode)];
-
-    DEBUG_PRINT("keysym: %x\n", keysym)
-    DEBUG_PRINT("modifiers: %i\n", event->state)
-
-    for (iterator = key_bindings; iterator != NULL; iterator = iterator->next)
-    {
-        binding = (struct velox_key_binding *) iterator->data;
-
-        if (keysym == binding->key->keysym &&
-            ((binding->key->modifiers == XCB_MOD_MASK_ANY) ||
-            (CLEAN_MASK(event->state) == binding->key->modifiers)))
-        {
-            if (binding->function != NULL)
-            {
-                binding->function(binding->arg);
-            }
-        }
-    }
-}
-
-static void mapping_notify(xcb_mapping_notify_event_t * event)
-{
-    DEBUG_ENTER
-
-    if (event->request == XCB_MAPPING_KEYBOARD)
-    {
-        grab_keys(event->first_keycode, event->first_keycode + event->count - 1);
-    }
-}
-
-static void map_request(xcb_map_request_event_t * event)
-{
-    struct velox_window * maybe_window;
-    xcb_get_window_attributes_cookie_t window_attributes_cookie;
-    xcb_get_window_attributes_reply_t * window_attributes;
-
-    DEBUG_ENTER
-
-    window_attributes_cookie = xcb_get_window_attributes(c, event->window);
-
-    maybe_window = tags_lookup_window(event->window);
-
-    window_attributes = xcb_get_window_attributes_reply(c, window_attributes_cookie, NULL);
-
-    if (window_attributes)
-    {
-        if (!maybe_window && !window_attributes->override_redirect)
-        {
-            manage(event->window);
-        }
-
-        free(window_attributes);
-    }
-}
-
 static void property_notify(xcb_property_notify_event_t * event)
 {
     xcb_get_atom_name_cookie_t atom_name_cookie;
@@ -249,38 +274,13 @@ static void property_notify(xcb_property_notify_event_t * event)
     free(atom_name_reply);
 }
 
-static void unmap_notify(xcb_unmap_notify_event_t * event)
+static void mapping_notify(xcb_mapping_notify_event_t * event)
 {
-    struct velox_window * window;
-
     DEBUG_ENTER
 
-    if (pending_unmaps > 0)
+    if (event->request == XCB_MAPPING_KEYBOARD)
     {
-        /* If we are expecting an unmap due to hiding a window, ignore it */
-        pending_unmaps--;
-        return;
-    }
-
-    window = window_loop_lookup(tag->windows, event->window);
-
-    if (window != NULL)
-    {
-        uint32_t property_values[2];
-
-        DEBUG_PRINT("setting state to withdrawn\n")
-
-        xcb_grab_server(c);
-
-        property_values[0] = XCB_WM_STATE_WITHDRAWN;
-        property_values[1] = 0;
-        xcb_change_property(c, XCB_PROP_MODE_REPLACE, window->window_id, WM_STATE, WM_STATE, 32, 2, property_values);
-
-        unmanage(window);
-
-        xcb_flush(c);
-
-        xcb_ungrab_server(c);
+        grab_keys(event->first_keycode, event->first_keycode + event->count - 1);
     }
 }
 
@@ -288,17 +288,11 @@ void handle_event(xcb_generic_event_t * event)
 {
     switch (event->response_type & ~0x80)
     {
+        case XCB_KEY_PRESS:
+            key_press((xcb_key_press_event_t *) event);
+            break;
         case XCB_BUTTON_PRESS:
             button_press((xcb_button_press_event_t *) event);
-            break;
-        case XCB_CONFIGURE_REQUEST:
-            configure_request((xcb_configure_request_event_t *) event);
-            break;
-        case XCB_CONFIGURE_NOTIFY:
-            configure_notify((xcb_configure_notify_event_t *) event);
-            break;
-        case XCB_DESTROY_NOTIFY:
-            destroy_notify((xcb_destroy_notify_event_t *) event);
             break;
         case XCB_ENTER_NOTIFY:
             enter_notify((xcb_enter_notify_event_t *) event);
@@ -306,20 +300,26 @@ void handle_event(xcb_generic_event_t * event)
         case XCB_LEAVE_NOTIFY:
             leave_notify((xcb_leave_notify_event_t *) event);
             break;
-        case XCB_KEY_PRESS:
-            key_press((xcb_key_press_event_t *) event);
+        case XCB_DESTROY_NOTIFY:
+            destroy_notify((xcb_destroy_notify_event_t *) event);
             break;
-        case XCB_MAPPING_NOTIFY:
-            mapping_notify((xcb_mapping_notify_event_t *) event);
+        case XCB_UNMAP_NOTIFY:
+            unmap_notify((xcb_unmap_notify_event_t *) event);
             break;
         case XCB_MAP_REQUEST:
             map_request((xcb_map_request_event_t *) event);
             break;
+        case XCB_CONFIGURE_NOTIFY:
+            configure_notify((xcb_configure_notify_event_t *) event);
+            break;
         case XCB_PROPERTY_NOTIFY:
             property_notify((xcb_property_notify_event_t *) event);
             break;
-        case XCB_UNMAP_NOTIFY:
-            unmap_notify((xcb_unmap_notify_event_t *) event);
+        case XCB_CLIENT_MESSAGE:
+            client_message((xcb_client_message_event_t *) event);
+            break;
+        case XCB_MAPPING_NOTIFY:
+            mapping_notify((xcb_mapping_notify_event_t *) event);
             break;
 
         default:
